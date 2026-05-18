0366b6349d987092722016ee84f23eda375f6d22
churit <churit@naver.com>
2026-05-18 20:05:19 +0900

    ㅋㅁ

--- src/mocks/adsMock.js
+++ src/mocks/adsMock.js
@@ -1,5 +1,7 @@
 // Web mock for react-native-google-mobile-ads
 export const RewardedAd = null;
 export const RewardedAdEventType = null;
+export const InterstitialAd = null;
+export const AdEventType = null;
 export const TestIds = null;
 export default null;
